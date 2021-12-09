b30949c656ac0d89fd1b290872db42ac56f81abe
ChrisH <steveage.ch@gmail.com>
2021-12-09 15:21:16 +0000

    day 1 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,54 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var prevDepth: Int = -1
+        var depthIncreases: Int = 0
+        for (DepthStr in input) {
+            val depth = DepthStr.toInt()
+            if (prevDepth == -1){
+                prevDepth = depth
+                continue
+            }
+            val diff = depth - prevDepth
+            if (diff > 0){
+                depthIncreases++
+            }
+            prevDepth = depth
+        }
+        return depthIncreases
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val depthInts = input.map{it.toInt()}
+
+        fun movingAverage(index: Int): Int{
+            // Get moving average, from given index back 2
+            return depthInts[index] + depthInts[index-1] + depthInts[index-2]
+        }
+
+        var depthIncreases = 0
+        for ((i, depth) in depthInts.withIndex()) {
+           if (i < 3){
+               continue  // Need 2 full moving averages
+           }
+            val currAverage = movingAverage(i)
+            val prevAverage = movingAverage(i-1)
+            val diff = currAverage - prevAverage
+            if (diff > 0){
+             depthIncreases++
+            }
+
+        }
+        return depthIncreases
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val TestRes = part1(testInput)
+    println("Test result: $TestRes")
+    check(TestRes == 7)
+
+    val TestResPart2 = part2(testInput)
+    check(TestResPart2 == 5)
 
     val input = readInput("Day01")
     println(part1(input))
