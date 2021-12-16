cec0aa12481a6499aa0f0df207c2c0d7b0fc1140
ChrisH <steveage.ch@gmail.com>
2021-12-16 14:36:54 +0000

    ran super fast for part 2.
    first answer was wrong, just converting everything to use longs
    solved

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,17 +1,17 @@
 fun main() {
 
-    val newFishValue = 8
-    val postSpawnValue = 6
+    val newFishValue: Long = 8
+    val postSpawnValue: Long = 6
 
-    fun incrementHashmap(map: MutableMap<Int, Int>, key: Int, incrementAmmount: Int = 1){
+    fun incrementHashmap(map: MutableMap<Long, Long>, key: Long, incrementAmmount: Long = 1){
         val count = map.getOrDefault(key, 0)
         map[key] = count + incrementAmmount
     }
 
-    fun calculateNextGen(currGen: HashMap<Int, Int>): HashMap<Int, Int>{
-        val newGen = HashMap<Int, Int>()
+    fun calculateNextGen(currGen: HashMap<Long, Long>): HashMap<Long, Long>{
+        val newGen = HashMap<Long, Long>()
         for ((key, value) in currGen){
-            if (key == 0){
+            if (key == 0.toLong()){
                 // New Fish spawn
                 incrementHashmap(newGen, newFishValue, incrementAmmount = value)
                 // Existing fish reset
@@ -24,43 +24,59 @@ fun main() {
     }
 
 
-   fun part1(input: List<String>): Int {
-       val initialFish = input[0].split(",").map { it.toInt() }
+//   fun part1(input: List<String>): Long {
+//       val initialFish = input[0].split(",").map { it.toInt() }
+//
+//       val generationFish: MutableList<HashMap<Int, Int>> = mutableListOf()
+//
+//       val initialGen = hashMapOf<Int, Int>()
+//       for (fishCurr in initialFish){
+//           incrementHashmap(initialGen, fishCurr)
+//       }
+//
+//       generationFish.add(initialGen)
+//
+//       for (i in 1..80){
+//           generationFish.add(
+//               calculateNextGen(generationFish[i-1])
+//           )
+//       }
+//       return generationFish.last().values.sum()
+//    }
 
-       val generationFish: MutableList<HashMap<Int, Int>> = mutableListOf()
+    fun part2(input: List<String>): Long {
+        val initialFish = input[0].split(",").map { it.toLong() }
 
-       val initialGen = hashMapOf<Int, Int>()
-       for (fishCurr in initialFish){
-           incrementHashmap(initialGen, fishCurr)
-       }
+        val generationFish: MutableList<HashMap<Long, Long>> = mutableListOf()
 
-       generationFish.add(initialGen)
+        val initialGen = hashMapOf<Long, Long>()
+        for (fishCurr in initialFish){
+            incrementHashmap(initialGen, fishCurr)
+        }
 
-       for (i in 1..80){
-           generationFish.add(
-               calculateNextGen(generationFish[i-1])
-           )
-       }
-       return generationFish.last().values.sum()
-    }
+        generationFish.add(initialGen)
 
-    fun part2(input: List<String>): Int {
-        return -1
+        for (i in 1..256){
+            generationFish.add(
+                calculateNextGen(generationFish[i-1])
+            )
+        }
+        return generationFish.last().values.sum()
     }
 
     // Change these for each day
     val day: String = "06"
     val testPart1Expected = 5934
-    val testPart2Expected = 12
+    val testPart2Expected = 26984457539
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day${day}_test")
-    val testResPart1 = part1(testInput)
-    check(testResPart1 == testPart1Expected){"Test for part 1 returned ${testResPart1}, expected $testPart1Expected"}
+//    val testResPart1 = part1(testInput)
+//    check(testResPart1 == testPart1Expected){"Test for part 1 returned ${testResPart1}, expected $testPart1Expected"}
     val input = readInput("Day${day}")
-    println("Part 1 result: ${part1(input)}")
+//    println("Part 1 result: ${part1(input)}")
 
     val testResPart2 = part2(testInput)
-    check(testResPart2 == testPart2Expected){"Test for part 2 returned $testResPart2, expected $testPart2Expected"}
+    check(testResPart2.toLong() == testPart2Expected){"Test for part 2 returned $testResPart2, expected $testPart2Expected"}
     println("Part 2 result: ${part2(input)}")
 }
